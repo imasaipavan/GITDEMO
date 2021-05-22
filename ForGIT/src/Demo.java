99028298298663fec79ad4310fc6e52c3dcd5bfc
kalyan kadali <kalyan kadali@DESKTOP-I6TKHMQ>
2021-05-22 19:03:16 +0530

    TO practive Push Operation

--- ForGIT/src/Demo.java
+++ ForGIT/src/Demo.java
@@ -5,7 +5,8 @@ public class Demo {
 	public static void main(String[] args) {
 		System.out.println("THIS IS DEMO FOR GIT");
 		System.out.println("FOR STAGING AREA");
-        System.out.println("FOR PULL OPERATION");			
+        System.out.println("FOR PULL OPERATION");
+        System.out.println("For Push Operation")
 		}
 	}
 
