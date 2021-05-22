67253f0444064cd45166069703b6be28799593d1
imavmsaipavan <79898987+imavmsaipavan@users.noreply.github.com>
2021-05-22 18:36:44 +0530

    FOR PULL OPERATION

--- ForGIT/src/Demo.java
+++ ForGIT/src/Demo.java
@@ -5,5 +5,7 @@ public class Demo {
 	public static void main(String[] args) {
 		System.out.println("THIS IS DEMO FOR GIT");
 		System.out.println("FOR STAGING AREA");
+System.out.println("FOR PULL OPERATION");			
+		}
 	}
 }
