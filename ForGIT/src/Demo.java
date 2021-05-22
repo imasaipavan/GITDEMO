98427588f7703250bc25e7c70d5ffd89923ba8aa
imavmsaipavan <79898987+imavmsaipavan@users.noreply.github.com>
2021-05-22 19:13:51 +0530

    for branch operation example

--- ForGIT/src/Demo.java
+++ ForGIT/src/Demo.java
@@ -6,6 +6,8 @@ public class Demo {
 		System.out.println("THIS IS DEMO FOR GIT");
 		System.out.println("FOR STAGING AREA");
         System.out.println("FOR PULL OPERATION");			
+        System.out.println("This is for Branch operation");
+       
 		}
 	}
 
